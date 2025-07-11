e3b210831b7a457502e4d5005873dd0391152d44
scx567888 <scx567888@outlook.com>
2025-07-11 11:37:02 +0800

    dev

--- src/main/java/cool/scx/reflect/ClassInfo.java
+++ src/main/java/cool/scx/reflect/ClassInfo.java
@@ -3,9 +3,9 @@ package cool.scx.reflect;
 import java.lang.reflect.AnnotatedElement;
 
 /// ClassInfo
-/// 
+///
 /// 注意 : 为了性能考虑 所有数组返回值都是直接暴漏的内部值 请不要修改
-/// 
+///
 /// @author scx567888
 /// @version 0.0.1
 public sealed interface ClassInfo extends TypeInfo, AccessModifierOwner, AnnotatedElementInfo permits ClassInfoImpl {
