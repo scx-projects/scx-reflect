c3ebf8747a85b4eb23e0711dac20a56830820831
scx567888 <scx567888@outlook.com>
2025-07-11 11:36:57 +0800

    dev

--- src/main/java/cool/scx/reflect/ClassInfo.java
+++ src/main/java/cool/scx/reflect/ClassInfo.java
@@ -1,11 +1,11 @@
 package cool.scx.reflect;
 
 import java.lang.reflect.AnnotatedElement;
-import java.lang.reflect.TypeVariable;
-import java.util.Map;
 
 /// ClassInfo
-///
+/// 
+/// 注意 : 为了性能考虑 所有数组返回值都是直接暴漏的内部值 请不要修改
+/// 
 /// @author scx567888
 /// @version 0.0.1
 public sealed interface ClassInfo extends TypeInfo, AccessModifierOwner, AnnotatedElementInfo permits ClassInfoImpl {
--- src/main/java/cool/scx/reflect/TypeBindings.java
+++ src/main/java/cool/scx/reflect/TypeBindings.java
@@ -5,22 +5,24 @@ import java.util.Map;
 
 /// TypeBindings
 ///
+/// 注意 : 为了性能考虑 所有数组返回值都是直接暴漏的内部值 请不要修改
+///
 /// @author scx567888
 /// @version 0.0.1
 public sealed interface TypeBindings extends Iterable<Map.Entry<TypeVariable<?>, TypeInfo>> permits TypeBindingsImpl {
-    
+
     TypeInfo get(TypeVariable<?> typeVariable);
-    
+
     TypeInfo get(String name);
-    
+
     TypeInfo get(int index);
 
     TypeVariable<?>[] typeVariables();
 
     TypeInfo[] typeInfos();
-    
+
     int size();
-    
+
     boolean isEmpty();
 
 }
