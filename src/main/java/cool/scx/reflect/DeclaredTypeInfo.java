17766413aca2ce864416bc57e5afdb4b224ed191
scx567888 <scx567888@outlook.com>
2025-07-12 19:19:43 +0800

    dev

--- src/main/java/cool/scx/reflect/DeclaredTypeInfo.java
+++ src/main/java/cool/scx/reflect/DeclaredTypeInfo.java
@@ -3,7 +3,7 @@ package cool.scx.reflect;
 import java.lang.reflect.AnnotatedElement;
 
 /// 声明类型
-public sealed interface DeclaredTypeInfo extends TypeInfo, AccessModifierOwner, AnnotatedElementInfo permits InterfaceTypeInfo {
+public sealed interface DeclaredTypeInfo extends TypeInfo, AccessModifierOwner, AnnotatedElementInfo permits ClassTypeInfo, InterfaceTypeInfo, EnumTypeInfo, AnnotationTypeInfo, RecordTypeInfo {
 
     @Override
     default AnnotatedElement annotatedElement() {
