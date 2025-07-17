8107cb1b700c0480b468b353a6e19083b2d23528
scx567888 <scx567888@outlook.com>
2025-07-17 18:52:56 +0800

    dev

--- src/main/java/cool/scx/reflect/ScxReflect.java
+++ src/main/java/cool/scx/reflect/ScxReflect.java
@@ -65,7 +65,7 @@ public final class ScxReflect {
         }
         // 当存在上下文 bindings 时, ParameterizedType 中可能包含被替换的 TypeVariable, 因此不能直接使用 ParameterizedType 作为 key.
         // 为了实现严格的 "同一个类型 永远只对应同一个 TypeInfo",
-        // 我们使用包含上下文的 ClassInfoImpl 作为 key. 它携带了真正完整的 bindings
+        // 我们使用包含上下文的 ClassInfoImpl 作为 key. 它携带了真正完整的 bindings, 同时正确的实现了 equals 和 hashCode.
         // 虽然构建 ClassInfoImpl 看似重复, 但它创建是轻量的, 并且后续可以作为 cache key 和最终值双重使用, 避免多次构建.
         // 而且 实际上当代码走到这里的时候 只可能是 正在初始化 ClassInfoImpl 内部的对象, 诸如 FieldInfo, MethodInfo 等.
         // 而这些对象 实际上是会被 ClassInfoImpl 内部缓存起来的, 这意味着 以下的代码实际上 并不会执行很多次, 性能不至于成为问题.
@@ -81,7 +81,10 @@ public final class ScxReflect {
     }
 
     private static TypeInfo getTypeFromGenericArrayType(GenericArrayType genericArrayType, TypeBindings contextBindings) {
-        //如果上下文 bindings 为 空, 消解为 无上下文 bindings 的版本
+        // 当上下文 bindings 为空时, GenericArrayType 是可以安全地作为 key 的.
+        // 因为没有上下文替换, 其组件类型是确定的, 我们可以直接使用其作为缓存 key.
+        // 同时可以尝试优化缓存: 
+        // 若组件类型不包含泛型 (即是基本类型或非泛型类), 我们还可以将其 rawClass 作为额外的 key, 进行双重缓存.
         if (contextBindings == EMPTY_BINDINGS) {
             // 使用原始 GenericArrayType 作为 Key, 这里是安全的, 因为我们没有上下文 bindings 也就不会发生替换
             var t = TYPE_CACHE.get(genericArrayType);
@@ -92,12 +95,12 @@ public final class ScxReflect {
             // 这里尝试复用或提前缓存, 只有组件类型是没有任何泛型的情况下 我们才可能复用
             return tryOptimizeCache(arrayTypeInfo, genericArrayType);
         }
-        // 我们直接使用一个 ArrayTypeInfoImpl 来实现现有类型的查找, 并同时使用这个作为后续存储的 key
-        // 注意 ArrayTypeInfoImpl 的创建实际上是轻量的, 而且携带了真正完整的 bindings
-        // 为了实现严格的 "同一个类型永远可以只获得同一个 TypeInfo",
-        // 实际上当代码走到这里的时候 只可能是 正在初始化 ClassInfoImpl 内部的 诸如 FieldInfo, MethodInfo 等.
-        // 而这些对象 实际上是会被 ClassInfoImpl 缓存起来的, 这意味着 以下的代码实际上 并不会执行很多次.
-        // 性能不至于成为问题
+        // 当存在上下文 bindings 时, GenericArrayType 中可能包含被替换的 TypeVariable, 因此不能直接使用 GenericArrayType 作为 key.
+        // 为了实现严格的 "同一个类型 永远只对应同一个 TypeInfo",
+        // 我们使用包含上下文的 ArrayTypeInfoImpl 作为 key. 它携带了真正完整的 bindings, 同时正确的实现了 equals 和 hashCode.
+        // 虽然构建 ArrayTypeInfoImpl 看似重复, 但它创建是轻量的, 并且后续可以作为 cache key 和最终值双重使用, 避免多次构建.
+        // 而且 实际上当代码走到这里的时候 只可能是 正在初始化 ClassInfoImpl 内部的对象, 诸如 FieldInfo, MethodInfo 等.
+        // 而这些对象 实际上是会被 ClassInfoImpl 内部缓存起来的, 这意味着 以下的代码实际上 并不会执行很多次, 性能不至于成为问题.
         var arrayTypeInfo = new ArrayTypeInfoImpl(genericArrayType, contextBindings);
         var typeInfo = TYPE_CACHE.get(arrayTypeInfo);
         if (typeInfo != null) {
@@ -124,7 +127,14 @@ public final class ScxReflect {
         return false;
     }
 
+    // 尝试优化缓存
     private static TypeInfo tryOptimizeCache(ArrayTypeInfoImpl arrayTypeInfo, Object typeKey) {
+        // 如果可以优化, 我们会将 arrayTypeInfo 同时缓存为 typeKey 和 rawClass 两份.
+        // 如果 rawClass 已经存在缓存, 说明此前已有等价类型被缓存, 我们直接复用旧的.
+        // 否则, 将当前类型写入两个 key.
+        // 如果无法优化, 则仅以 typeKey 进行缓存.
+        // 这个优化不单单是 为了性能, 同时也保证了 同一个类型拿到的 TypeInfo 永远是一致的, 
+        // 无论是先通过 Class 创建, 还是先通过 GenericArrayType 创建, 最终的 TypeInfo 是一致的.
         var canOptimize = canReuseRawClass(arrayTypeInfo);
         if (canOptimize) {
             var oldTypeInfo = TYPE_CACHE.get(arrayTypeInfo.rawClass());
@@ -146,7 +156,8 @@ public final class ScxReflect {
     }
 
     private static TypeInfo getTypeFromTypeVariable(TypeVariable<?> typeVariable, TypeBindings contextBindings) {
-        //尝试从从绑定中获取 否则回退到 上界
+        // 尝试从上下文 bindings 获取已绑定的类型变量类型, 若无法获取，则使用其上界 (第一个 bound) 进行退化处理.
+        // 这种退化是合理且安全的, 因为在 Java 泛型系统中也是这么退化的.
         var typeInfo = contextBindings.get(typeVariable);
         if (typeInfo != null) {
             return typeInfo;
@@ -155,7 +166,8 @@ public final class ScxReflect {
     }
 
     private static TypeInfo getTypeFromWildcardType(WildcardType wildcardType, TypeBindings contextBindings) {
-        // 回退到上界
+        // 通配符类型理论上 还具有下界, 但是我们在反射系统中通常只想知道 "这个类型到底能存储什么".
+        // 所以此处忽略下界, 直接退化为上界.
         return getTypeFromAny(wildcardType.getUpperBounds()[0], contextBindings);
     }
 
