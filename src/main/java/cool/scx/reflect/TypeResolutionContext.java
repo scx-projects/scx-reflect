b79f820e866fbc0a31fdd7db65bad4ffc859164d
scx567888 <scx567888@outlook.com>
2025-07-17 23:23:58 +0800

    dev

--- src/main/java/cool/scx/reflect/TypeResolutionContext.java
+++ src/main/java/cool/scx/reflect/TypeResolutionContext.java
@@ -5,6 +5,17 @@ import java.util.IdentityHashMap;
 import java.util.Map;
 
 /// 解析上下文
+/// 
+/// 理论上 是可能存在 一个 ParameterizedType 的泛型参数 是自身的情况
+/// 比如一个类定义如下:
+/// public class Node<T extends Node<T>> {
+///
+/// }
+///
+/// Type type = Node.class.getTypeParameters()[0].getBounds()[0];
+///
+/// 这时 type 的 泛型参数实际上就是 自引用的
+/// 此处我们 通过 在 ClassInfoImpl 构造函数中 提前缓存半成品 的方式来部分绕过.
 final class TypeResolutionContext {
 
     private final TypeBindings bindings;
