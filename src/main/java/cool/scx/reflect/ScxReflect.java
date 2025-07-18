49e348d44ccef94064c153bfc05c811e2ae3d162
scx567888 <scx567888@outlook.com>
2025-07-18 08:11:34 +0800

    dev

--- src/main/java/cool/scx/reflect/ScxReflect.java
+++ src/main/java/cool/scx/reflect/ScxReflect.java
@@ -1,5 +1,7 @@
 package cool.scx.reflect;
 
+import java.lang.reflect.Type;
+
 import static cool.scx.reflect.TypeBindingsImpl.EMPTY_BINDINGS;
 import static cool.scx.reflect.TypeFactory.getTypeFromAny;
 import static cool.scx.reflect.TypeFactory.getTypeFromClass;
@@ -8,12 +10,42 @@ public final class ScxReflect {
 
     /// 根据 Class 获取 TypeInfo
     public static TypeInfo getType(Class<?> type) {
-        return getTypeFromClass(type);
+        var sss= getTypeFromClass(type);
+        //todo 测试代码
+        if (sss instanceof ClassInfo classInfo) {
+            classInfo.allFields();
+            classInfo.allMethods();
+            classInfo.allInterfaces();
+            classInfo.allSuperClasses();    
+        }
+        return sss;
+    }
+
+
+    /// 根据 Class 获取 TypeInfo
+    public static TypeInfo getType(Type type) {
+        var sss= getTypeFromAny(type,new TypeResolutionContext(EMPTY_BINDINGS));
+        //todo 测试代码
+        if (sss instanceof ClassInfo classInfo) {
+            classInfo.fields();
+            classInfo.methods();
+            classInfo.superClass();
+            classInfo.interfaces();
+        }
+        return sss;
     }
 
     /// 根据 TypeReference 获取 TypeInfo
     public static TypeInfo getType(TypeReference<?> typeReference) {
-        return getTypeFromAny(typeReference.getType(), EMPTY_BINDINGS);
+        var sss= getTypeFromAny(typeReference.getType(), new TypeResolutionContext(EMPTY_BINDINGS));
+        //todo 测试代码
+        if (sss instanceof ClassInfo classInfo) {
+            classInfo.fields();
+            classInfo.methods();
+            classInfo.superClass();
+            classInfo.interfaces();
+        }
+        return sss;
     }
 
 }
