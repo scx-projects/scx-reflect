bee5ea62453fb00a932999f4b03fc814b4b383c3
scx567888 <scx567888@outlook.com>
2025-07-22 11:09:51 +0800

    代码优化

--- src/main/java/cool/scx/reflect/ArrayTypeInfo.java
+++ src/main/java/cool/scx/reflect/ArrayTypeInfo.java
@@ -1,5 +1,7 @@
 package cool.scx.reflect;
 
+import java.lang.reflect.Array;
+
 /// ArrayTypeInfo
 ///
 /// @author scx567888
@@ -8,5 +10,11 @@ public sealed interface ArrayTypeInfo extends TypeInfo permits ArrayTypeInfoImpl
 
     /// 组件类型, 此处只表示 当前组件的类型, 即对于多维数组 例如 String[][] 再此处拿到的是 String[]
     TypeInfo componentType();
+
+    /// 创建数组
+    @SuppressWarnings("unchecked")
+    default <A> A newArray(int size) {
+        return (A) Array.newInstance(this.componentType().rawClass(), size);
+    }
 
 }
--- src/main/java/cool/scx/reflect/TypeFactory.java
+++ src/main/java/cool/scx/reflect/TypeFactory.java
@@ -137,7 +137,7 @@ final class TypeFactory {
         }
     }
 
-    private static TypeInfo typeOfGenericArrayType(GenericArrayType genericArrayType, TypeResolutionContext context) {
+    public static TypeInfo typeOfGenericArrayType(GenericArrayType genericArrayType, TypeResolutionContext context) {
         // 如果上下文 bindings 为空, 则可直接使用原始 GenericArrayType 作为 key.
         // 这是安全的, 因为即使其中包含 TypeVariable 或 WildcardType, 也会因 bindings 为空而退化为其上界, 结果是确定的.
         // 因此, 在无上下文 bindings 的场景下, 同一个 GenericArrayType 实例总是可以映射到同一个 TypeInfo.
