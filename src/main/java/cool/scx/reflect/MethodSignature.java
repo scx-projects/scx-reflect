504ee4fb836b3f0a8730df1d0f65c5ac1d793a3f
scx567888 <scx567888@outlook.com>
2025-07-28 12:56:29 +0800

    dev

--- src/main/java/cool/scx/reflect/MethodSignature.java
+++ src/main/java/cool/scx/reflect/MethodSignature.java
@@ -3,16 +3,36 @@ package cool.scx.reflect;
 import java.util.Arrays;
 import java.util.Objects;
 
+import static cool.scx.reflect.ReflectSupport._findParameterTypes;
+
+/// MethodSignature
+///
+/// @author scx567888
+/// @version 0.0.1
 public final class MethodSignature {
 
+    private final MethodInfo methodInfo;
     private final String name;
     private final Class<?>[] parameterTypes;
     private final int hashCode;
 
     public MethodSignature(MethodInfo methodInfo) {
+        this.methodInfo = methodInfo;
         this.name = methodInfo.name();
-        this.parameterTypes = methodInfo.rawMethod().getParameterTypes();
+        this.parameterTypes = _findParameterTypes(methodInfo);
         this.hashCode = this._hashCode();
+    }
+
+    public MethodInfo methodInfo() {
+        return methodInfo;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Class<?>[] parameterTypes() {
+        return parameterTypes.clone();
     }
 
     @Override
@@ -35,6 +55,18 @@ public final class MethodSignature {
     @Override
     public int hashCode() {
         return hashCode;
+    }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        sb.append(name);
+        // 参数列表
+        sb.append("(");
+        var paramsStr = Arrays.stream(parameterTypes).map(Class::getSimpleName).toList();
+        sb.append(String.join(", ", paramsStr));
+        sb.append(")");
+        return sb.toString();
     }
 
 }
